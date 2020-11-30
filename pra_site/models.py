cc00d1114899b5bd296e897482d63924ff81a108
Hung Nguyen <quochung.nguyen@tieto.com>
2020-11-30 20:32:31 +0200

    Add representation for Project object

--- pra_site/models.py
+++ pra_site/models.py
@@ -5,4 +5,7 @@ class Project(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     sonarcloud_link = db.Column(db.String, unique=True, nullable=False)
     jenkins_link = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Project('{self.name}')"
     
