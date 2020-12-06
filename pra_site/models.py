104d1d260ccbeecf90009c023efc98f84914c6c4
Hung Nguyen <quochung.nguyen@tieto.com>
2020-12-06 11:23:24 +0200

    Adding Source object to db

--- pra_site/models.py
+++ pra_site/models.py
@@ -2,9 +2,12 @@ from pra_site import db
 
 class Source(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sonar_org_key = db.Column(db.String, unique=True, nullable=False)
-    jenkins_server = db.Column(db.String, unique=True, nullable=False)
+    sonar_org_key = db.Column(db.String, unique=False, nullable=False)
+    jenkins_server = db.Column(db.String, unique=False, nullable=False)
+    # to be updated later by data processing pipelines
+    # latest_processed_date
+     
 
-    # def __repr__(self):
-    #     return f"Project('{self.name}')"
+    def __repr__(self):
+        return f"Source('{self.sonar_org_key}' - '{self.jenkins_server}')"
     
--- pra_site/routes.py
+++ pra_site/routes.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template, flash, url_for, redirect
 
 from pra_site.forms import InputForm
 from pra_site.models import Source
-from pra_site import app
+from pra_site import app, db
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = InputForm()
     if form.validate_on_submit():
-        flash(f'Your link is registered successfully.', 'success')
+        sonar_org_key = form.sonar_org_key.data
+        jenkins_server = form.jenkins_server.data
+
+        sources = Source.query.filter_by(sonar_org_key=sonar_org_key).all()
+        # Servers corresponding to thi Sonar Organization
+        servers = list(map(lambda s: s.jenkins_server, sources))
+        # not yet registered
+        if sources == [] or jenkins_server not in servers:
+            # Add to DB
+            source = Source(sonar_org_key=sonar_org_key, jenkins_server=jenkins_server)
+            db.session.add(source)
+            db.session.commit()
+            flash(f'Your link is registered successfully.', 'success')
+        else:
+            flash(f"Your combination of Sonarcloud organization key and Jenkins server is already registered.", 'success')
+
         return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
 
