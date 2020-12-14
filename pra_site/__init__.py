eb6e07e1d44ae769c4f8134f8f06aea6f0e6c2b2
Hung Nguyen <hungnguyen10897@gmail.com>
2020-12-14 19:49:59 +0200

    Load config from /etc/pra-site-config.json instead of hard coding

--- pra_site/__init__.py
+++ pra_site/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+import json
+
+with open("/etc/pra-site-config.json", "r") as f:
+    config = json.load(f)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://pra:pra@localhost/pra2'
+app.config['SECRET_KEY'] = config["SECRET_KEY"]
+app.config['SQLALCHEMY_DATABASE_URI'] = config["SQLALCHEMY_DATABASE_URI"]
 db = SQLAlchemy(app)
 
 from pra_site import routes
